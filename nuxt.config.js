a221cb522f5cb736c7a2a01cfea55cdcdb0b1b0d
Jeric de Leon <jdeleon@doximity.com>
2021-11-02 09:14:59 +0800

    Add env

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,5 +44,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+
+  privateRuntimeConfig: {
+    bugsnagApiKey: process.env.BUGSNAG_API_KEY
   }
 }
