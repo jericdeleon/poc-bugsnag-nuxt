c490e605c2df32939ff8885a1fe53acf6d0d4b37
Jeric de Leon <jdeleon@doximity.com>
2021-11-02 14:07:22 +0800

    Add metadata: roomKey, rating

--- plugins/bugsnag.js
+++ plugins/bugsnag.js
@@ -1,6 +1,12 @@
 import Bugsnag from '@bugsnag/js'
 import BugsnagPluginVue from '@bugsnag/plugin-vue'
 
+function getRandomIntInclusive (min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1) + min) // The maximum is inclusive and the minimum is inclusive
+}
+
 export default function ({ $config: { bugsnagApiKey } }, inject) {
   if (process.server) {
     return null
@@ -8,7 +14,18 @@ export default function ({ $config: { bugsnagApiKey } }, inject) {
 
   const bugsnagClient = Bugsnag.start({
     apiKey: bugsnagApiKey,
-    plugins: [new BugsnagPluginVue()]
+    plugins: [new BugsnagPluginVue()],
+    onError (event) {
+      const path = new URL(event.request.url).pathname
+      const roomKey = path.split('/').pop()
+
+      if (roomKey) {
+        event.addMetadata('dialer', {
+          roomKey,
+          rating: getRandomIntInclusive(1, 5)
+        })
+      }
+    }
   })
 
   inject('bugsnag', bugsnagClient)
