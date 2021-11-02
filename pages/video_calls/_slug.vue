ff917060f53c6d47a150e4b24dbe960984b84252
Jeric de Leon <jdeleon@doximity.com>
2021-11-02 08:51:53 +0800

    Trigger manual errors

---
+++ pages/video_calls/_slug.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>
+      Trigger error for {{ slug }}:
+    </h1>
+    <div>
+      <button @click="randomError()">
+        Random Error
+      </button>
+      <button @click="differentError()">
+        Different Error
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData ({ params }) {
+    const slug = params.slug
+    return { slug }
+  },
+  methods: {
+    randomError () {
+      throw new Error('RandomError')
+    },
+    differentError () {
+      throw new Error('DifferentError')
+    }
+  }
+}
+</script>
+
+<style>
+</style>
